97ea444ee5f49278d74e66197b6a260f68202e90
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2022-11-15 22:56:45 +0000

    Wrote code for NumStack to satisfy JUnit test NumStackTest.

--- src/main/java/calculator/NumStack.java
+++ src/main/java/calculator/NumStack.java
@@ -2,20 +2,61 @@ package calculator;
 
 import java.util.EmptyStackException;
 
-public class NumStack{
+/**
+ * This is a facade class that holds numeric values in a {@link calculator.Stack} and hides the
+ * details of the {@link calculator.Stack} class.
+ * 
+ * @author wjis132
+ *
+ */
+public class NumStack {
+
+  private Stack numStack;
+
+  /**
+   * Default constructor for {@link calculator.NumStack} class that initialises the
+   * {@link calculator.NumStack#numStack} as a {@link calculator.Stack}.
+   */
   public NumStack() {
-    super();
+    this.numStack = new Stack();
   }
 
+  /**
+   * Accepts a numeric value and adds it to the {@link calculator.NumStack#numStack}.
+   * 
+   * @param i Numeric value to be pushed into {@link calculator.NumStack#numStack}
+   */
   public void push(float i) {
-
+    this.numStack.push(new Entry(i));
   }
 
+  /**
+   * Gets the top {@link calculator.Entry} from {@link calculator.NumStack#numStack} removes it by
+   * calling pop(), get the numeric value from the {@link calculator.Entry} and returns it.
+   * 
+   * @return float The top numeric value from {@link calculator.NumStack#numStack} as float.
+   * @throws EmptyStackException Throws EmptyStackException when
+   *         {@link calculator.NumStack#numStack} is empty.
+   */
   public float pop() {
-    throw new EmptyStackException();
+    if (this.isEmpty()) {
+      throw new EmptyStackException();
+    } else {
+      try {
+        return this.numStack.pop().getValue();
+      } catch (BadTypeException e) {
+        e.printStackTrace();
+      }
+      return 0;
+    }
   }
 
+  /**
+   * Returns true if the {@link calculator.NumStack#numStack} is empty, false if it is not empty.
+   * 
+   * @return Boolean
+   */
   public Boolean isEmpty() {
-    return true;
+    return this.numStack.size() == 0 ? true : false;
   }
 }
