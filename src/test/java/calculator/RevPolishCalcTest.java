214241a07c39965c47827a20a96ae13972537674
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2022-11-23 13:33:20 +0000

    Written more tests for RevPolishCalc.

--- src/test/java/calculator/RevPolishCalcTest.java
+++ src/test/java/calculator/RevPolishCalcTest.java
@@ -3,7 +3,6 @@ package calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.EmptyStackException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +18,7 @@ class RevPolishCalcTest {
   @Test
   void testEvaluateNull() {
     // Entering null as expression should throw InvalidExpressionException
-    assertThrows(EmptyStackException.class, () -> {
+    assertThrows(InvalidExpressionException.class, () -> {
       this.rpCalc.evaluate(null);
     });
   }
@@ -27,7 +26,7 @@ class RevPolishCalcTest {
   @Test
   void testEvaluateNumberOnly() {
     // Entering only one number should return same number
-    assertEquals(0, this.rpCalc.evaluate("0"));
+    assertEquals(1, this.rpCalc.evaluate("1"));
   }
 
   @Test
@@ -51,7 +50,7 @@ class RevPolishCalcTest {
   @Test
   void testEvaluateDivide() {
     // Entering a simple division should return correct result
-    assertEquals(1 / 2, this.rpCalc.evaluate("1 2 /"));
+    assertEquals(1f / 2f, this.rpCalc.evaluate("1 2 /"));
   }
 
   @Test
@@ -75,7 +74,7 @@ class RevPolishCalcTest {
   @Test
   void testEvaluateDivideTwice() {
     // Two additions should be evaluated in order
-    assertEquals(1 / 2 / 3, this.rpCalc.evaluate("1 2 / 3 /"));
+    assertEquals(1f / 2f / 3f, this.rpCalc.evaluate("1 2 / 3 /"));
   }
 
   @Test
@@ -87,14 +86,40 @@ class RevPolishCalcTest {
   @Test
   void testEvaluateAddDivide() {
     // Add before division
-    assertEquals((1 + 2) / 3, this.rpCalc.evaluate("1 2 + 3 /"));
+    assertEquals((1 + 2) / 3f, this.rpCalc.evaluate("1 2 + 3 /"));
   }
 
   @Test
   void testEvaluateMultipleOp() {
-    // Test Multiple operations in Reverse Polish Expression as in the example 
+    // Test Multiple operations in Reverse Polish Expression as in the example
     assertEquals((5 * (6 + 7)) - 2, this.rpCalc.evaluate("5 6 7 + * 2 -"));
     assertEquals(63, this.rpCalc.evaluate("5 6 7 + * 2 -"));
+  }
+
+  /**
+   * Test for {@link calculator.RevPolishCalc.evaluate} when there is inadequate operand(s)
+   */
+  @Test
+  void testEvaluateInadequateOperand() {
+    // rpCalc should throw exception when there is operation without operands
+    assertThrows(InvalidExpressionException.class, () -> {
+      this.rpCalc.evaluate("+");
+    });
+    // rpCalc should throw exception when there is only one operand for addition
+    assertThrows(InvalidExpressionException.class, () -> {
+      this.rpCalc.evaluate("1 +");
+    });
+  }
+
+  /**
+   * Test for {@link calculator.RevPolishCalc.evaluate} when there is inadequate operand(s)
+   */
+  @Test
+  void testEvaluateInvalidOp() {
+    // rpCalc should throw exception when there is invalid symbols i.e. brackets
+    assertThrows(InvalidExpressionException.class, () -> {
+      this.rpCalc.evaluate("(");
+    });
   }
 
 
