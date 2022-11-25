d1811744b90c320c38a6788cb1e40d0a3e43644b
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2022-11-25 22:18:58 +0000

    Wrote code for RevPolishCalc that passes RevPolishCalcTest.

--- src/main/java/calculator/RevPolishCalc.java
+++ src/main/java/calculator/RevPolishCalc.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.EmptyStackException;
+import java.util.Scanner;
+
 /**
  * Evaluates Reverse Polish String Expressions.
  * 
@@ -10,12 +13,6 @@ public class RevPolishCalc implements Calculator {
   private NumStack values;
 
   /**
-   * Constructor for {@link calculator.ReversePolishCalc} that intialises
-   * {@link calculator.RevPolishCalc#values} as {@link calculator.NumStack}.
-   */
-  public RevPolishCalc() {}
-
-  /**
    * Interface function for evaluating Reverse Polish string expressions.
    * 
    * @param what Reverse Polish string expressions for evaluation.
@@ -23,8 +20,59 @@ public class RevPolishCalc implements Calculator {
    * @throws InvalidExpressionException Thrown when the string expression is invalid.
    */
   @Override
-  public float evaluate(String what) {
-    return 0;
+  public float evaluate(String what) throws InvalidExpressionException {
+
+    if (what == null) {
+      // If expression is null, throw InvalidExpressionException
+      throw new InvalidExpressionException("Invalid Expression: Expression is null");
+    } else {
+
+      // If expression is not null, create a new NumStack
+      values = new NumStack();
+
+      // Split the incoming string expression to items with spaces
+      String[] items = what.split("\\s+");
+
+      // For each item, check the type: if number, push to stack; if operator, perform operation
+      for (int i = 0; i < items.length; i++) {
+        try {
+          // If parseFloat is successful, it means the item is indeed a number
+          float operand = Float.parseFloat(items[i]);
+          // Now push it into values
+          values.push(operand);
+        } catch (NumberFormatException nfe) {
+          try {
+            // NumberFormatException means item not number, possibly an operator
+            
+            // pop the top operand first
+            float topValue = values.pop();
+            
+            // Now check if it is a valid symbol (+ - * /)
+            switch (items[i]) {
+              case "+":
+                values.push(values.pop() + topValue);
+                break;
+              case "-":
+                values.push(values.pop() - topValue);
+                break;
+              case "*":
+                values.push(values.pop() * topValue);
+                break;
+              case "/":
+                values.push(values.pop() / topValue);
+                break;
+              default:
+                // if not valid symbol (+ - * /), would be invalid operator
+                throw new InvalidExpressionException("Invalid Expression: Invalid operator");
+            }
+          } catch (EmptyStackException ese) {
+            // Pop from stack throwing EmptyStackException means not enough operands for operation
+            throw new InvalidExpressionException("Invalid Expression: Not enough operands");
+          }
+        }
+      }
+      return values.pop();
+    }
   }
 
 }
--- src/test/java/calculator/RevPolishCalcTest.java
+++ src/test/java/calculator/RevPolishCalcTest.java
@@ -10,11 +10,17 @@ class RevPolishCalcTest {
 
   private RevPolishCalc rpCalc;
 
+  /**
+   * Initialises {@link RevPolishCalc.rpCalc} before each test.
+   */
   @BeforeEach
   void newRevPolishCalc() {
     this.rpCalc = new RevPolishCalc();
   }
 
+  /**
+   * Test 1: Evaluate null as expression
+   */
   @Test
   void testEvaluateNull() {
     // Entering null as expression should throw InvalidExpressionException
@@ -23,81 +29,165 @@ class RevPolishCalcTest {
     });
   }
 
+  /**
+   * Test 2: Evaluate expression with single number only
+   */
   @Test
   void testEvaluateNumberOnly() {
     // Entering only one number should return same number
-    assertEquals(1, this.rpCalc.evaluate("1"));
-  }
-
-  @Test
-  void testEvaluateAdd() {
-    // Entering a simple addition should return correct result
-    assertEquals(1 + 2, this.rpCalc.evaluate("1 2 +"));
-  }
-
-  @Test
-  void testEvaluateSubtract() {
-    // Entering a simple subtraction should return correct result
-    assertEquals(1 - 2, this.rpCalc.evaluate("1 2 -"));
-  }
-
-  @Test
-  void testEvaluateMultiply() {
-    // Entering a simple multiplication should return correct result
-    assertEquals(1 * 2, this.rpCalc.evaluate("1 2 *"));
-  }
-
-  @Test
-  void testEvaluateDivide() {
-    // Entering a simple division should return correct result
-    assertEquals(1f / 2f, this.rpCalc.evaluate("1 2 /"));
-  }
-
-  @Test
-  void testEvaluateAddTwice() {
-    // Two additions should be evaluated in order
-    assertEquals(1 + 2 + 3, this.rpCalc.evaluate("1 2 + 3 +"));
-  }
-
-  @Test
-  void testEvaluateSubtractTwice() {
-    // Two subtractions should be evaluated in order
-    assertEquals(1 - 2 - 3, this.rpCalc.evaluate("1 2 - 3 -"));
-  }
-
-  @Test
-  void testEvaluateMultiplyTwice() {
-    // Two multiplications should be evaluated in order
-    assertEquals(1 * 2 * 3, this.rpCalc.evaluate("1 2 * 3 *"));
-  }
-
-  @Test
-  void testEvaluateDivideTwice() {
-    // Two additions should be evaluated in order
-    assertEquals(1f / 2f / 3f, this.rpCalc.evaluate("1 2 / 3 /"));
-  }
-
-  @Test
-  void testEvaluateAddMultiply() {
-    // Add before Multiplication
-    assertEquals((1 + 2) * 3, this.rpCalc.evaluate("1 2 + 3 *"));
-  }
-
-  @Test
-  void testEvaluateAddDivide() {
-    // Add before division
-    assertEquals((1 + 2) / 3f, this.rpCalc.evaluate("1 2 + 3 /"));
-  }
-
-  @Test
-  void testEvaluateMultipleOp() {
-    // Test Multiple operations in Reverse Polish Expression as in the example
-    assertEquals((5 * (6 + 7)) - 2, this.rpCalc.evaluate("5 6 7 + * 2 -"));
-    assertEquals(63, this.rpCalc.evaluate("5 6 7 + * 2 -"));
+    try {
+      assertEquals(1, this.rpCalc.evaluate("1"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
-   * Test for {@link calculator.RevPolishCalc.evaluate} when there is inadequate operand(s)
+   * Test 3: Evaluate expression with simple addition
+   */
+  @Test
+  void testEvaluateAdd() {
+    // Entering a simple addition should return correct result
+    try {
+      assertEquals(1 + 2, this.rpCalc.evaluate("1 2 +"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 4: Evaluate expression with simple subtraction
+   */
+  @Test
+  void testEvaluateSubtract() {
+    // Entering a simple subtraction should return correct result
+    try {
+      assertEquals(1 - 2, this.rpCalc.evaluate("1 2 -"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 5: Evaluate expression with simple multiplication
+   */
+  @Test
+  void testEvaluateMultiply() {
+    // Entering a simple multiplication should return correct result
+    try {
+      assertEquals(1 * 2, this.rpCalc.evaluate("1 2 *"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 6: Evaluate expression with simple division
+   */
+  @Test
+  void testEvaluateDivide() {
+    // Entering a simple division should return correct result
+    try {
+      assertEquals(1f / 2f, this.rpCalc.evaluate("1 2 /"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 7: Evaluate expression with two additions
+   */
+  @Test
+  void testEvaluateAddTwice() {
+    // Two additions should be evaluated in order
+    try {
+      assertEquals(1 + 2 + 3, this.rpCalc.evaluate("1 2 + 3 +"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 8: Evaluate expression with two subtractions
+   */
+  @Test
+  void testEvaluateSubtractTwice() {
+    // Two subtractions should be evaluated in order
+    try {
+      assertEquals(1 - 2 - 3, this.rpCalc.evaluate("1 2 - 3 -"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 9: Evaluate expression with two multiplications
+   */
+  @Test
+  void testEvaluateMultiplyTwice() {
+    // Two multiplications should be evaluated in order
+    try {
+      assertEquals(1 * 2 * 3, this.rpCalc.evaluate("1 2 * 3 *"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 10: Evaluate expression with two divisions
+   */
+  @Test
+  void testEvaluateDivideTwice() {
+    // Two additions should be evaluated in order
+    try {
+      assertEquals(1f / 2f / 3f, this.rpCalc.evaluate("1 2 / 3 /"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 11: Evaluate expression with addition then multiplication
+   */
+  @Test
+  void testEvaluateAddMultiply() {
+    // Add before Multiplication
+    try {
+      assertEquals((1 + 2) * 3, this.rpCalc.evaluate("1 2 + 3 *"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 12: Evaluate expression with addition then division
+   */
+  @Test
+  void testEvaluateAddDivide() {
+    // Add before division
+    try {
+      assertEquals((1 + 2) / 3f, this.rpCalc.evaluate("1 2 + 3 /"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 13: Evaluate expression with multiple operations
+   */
+  @Test
+  void testEvaluateMultipleOp() {
+    try {
+      // Test Multiple operations in Reverse Polish Expression as in the example
+      assertEquals((5 * (6 + 7)) - 2, this.rpCalc.evaluate("5 6 7 + * 2 -"));
+      assertEquals(63, this.rpCalc.evaluate("5 6 7 + * 2 -"));
+    } catch (InvalidExpressionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Test 14: Evaluate expression with less than 2 operands
    */
   @Test
   void testEvaluateInadequateOperand() {
@@ -112,7 +202,7 @@ class RevPolishCalcTest {
   }
 
   /**
-   * Test for {@link calculator.RevPolishCalc.evaluate} when there is inadequate operand(s)
+   * Test 15: Evaluate expression with invalid operator
    */
   @Test
   void testEvaluateInvalidOp() {
