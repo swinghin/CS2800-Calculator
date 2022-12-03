d365db9c33a056dbbb30c24c0d8fec67d5e95935
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2022-12-03 00:21:57 +0000

    Clean up auto-generated files in 'target' folder.

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"calculator","l":"BadTypeException"},{"p":"calculator","l":"Entry"},{"p":"calculator","l":"Stack"},{"p":"calculator","l":"Symbol"},{"p":"calculator","l":"Type"}];updateSearchResults();
