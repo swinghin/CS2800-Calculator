f780643bb6eb4f3cc63aff9f24efc7f6102ac39d
wjis132 <wing.shih.2020@live.rhul.ac.uk>
2022-12-06 19:57:28 +0000

    Beautify the calculator GUI.

--- src/main/java/calculator/CalcController.java
+++ src/main/java/calculator/CalcController.java
@@ -1,9 +1,8 @@
 package calculator;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -65,8 +65,20 @@ public final class CalcController extends Application {
    * Label for {@link calculator.CalcController#txtResult} TextField.
    */
   private Label lblResult;
-
+  /**
+   * TextField for answers.
+   */
   private TextField txtResult;
+
+  /**
+   * Insets preset with 12px padding
+   */
+  private final Insets thickInset = new Insets(12f);
+
+  /**
+   * Insets preset with 6px padding
+   */
+  private final Insets thinInset = new Insets(6f);
 
 
   /**
@@ -74,8 +86,10 @@ public final class CalcController extends Application {
    */
   public void calculate() {
     try {
+      // If expression is not invalid, put answer to Result TextField
       txtResult.setText(String.valueOf(model.evaluate(txtExpression.getText(), isInfix)));
     } catch (InvalidExpressionException e) {
+      // If expression invalid, print the exception message to Result TextField
       txtResult.setText(e.getMessage());
     }
   }
@@ -84,7 +98,7 @@ public final class CalcController extends Application {
    * Is notified when a change of expression type is indicated.
    */
   public void expressionType() {
-    isInfix = !isInfix;
+    isInfix = !isInfix; // flips isInfix when selected toggle changes
   }
 
   /**
@@ -102,41 +116,78 @@ public final class CalcController extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
+    // Configure Expression Label
     lblExpression = new Label();
-    lblExpression.setText("Expression");
+    lblExpression.setPadding(thinInset);
+    lblExpression.setPrefWidth(100f);
+    lblExpression.setAlignment(Pos.CENTER_RIGHT);
+    lblExpression.setText("Expression:");
 
+    // Configure Expression TextField
     txtExpression = new TextField();
+    txtExpression.setPadding(thinInset);
     txtExpression.setPromptText("Enter an expression...");
+    HBox.setHgrow(txtExpression, Priority.ALWAYS);
 
+    // ToggleGroup for rbnRevPol and rbnInfix
     togIsInfix = new ToggleGroup();
 
+    // Configure Reverse Polish RadioButton
     rbnRevPol = new RadioButton();
+    rbnRevPol.setPadding(thinInset);
     rbnRevPol.setToggleGroup(togIsInfix);
     rbnRevPol.setText("Reverse Polish");
     rbnRevPol.setOnAction(event -> expressionType());
 
+    // Configure Infix RadioButton
     rbnInfix = new RadioButton();
+    rbnInfix.setPadding(thinInset);
     rbnInfix.setToggleGroup(togIsInfix);
     rbnInfix.setText("Infix");
     rbnInfix.setSelected(isInfix);
     rbnInfix.setOnAction(event -> expressionType());
 
+    // Configure Calculate Button
     btnCalculate = new Button();
+    btnCalculate.setPadding(thinInset);
     btnCalculate.setText("Calculate");
+    // call calculate() when button is pressed
     btnCalculate.setOnAction(event -> calculate());
 
+    // Configure Result Label
     lblResult = new Label();
-    lblResult.setText("Result");
+    lblResult.setPadding(thinInset);
+    lblResult.setPrefWidth(100f);
+    lblResult.setAlignment(Pos.CENTER_RIGHT);
+    lblResult.setText("Result:");
 
+    // Configure Result TextField
     txtResult = new TextField();
-    txtResult.setEditable(false);
+    txtResult.setPadding(thinInset);
+    txtResult.setEditable(false); // Not allow user to edit this field
     txtResult.setPromptText("Click 'Calculate' to get result...");
+    HBox.setHgrow(txtResult, Priority.ALWAYS);
 
+    HBox hbxExpression = new HBox(lblExpression, txtExpression);
+    hbxExpression.setPadding(thickInset);
+    hbxExpression.setAlignment(Pos.CENTER);
 
+    HBox hbxRadio = new HBox(rbnRevPol, rbnInfix);
+    hbxRadio.setPadding(thickInset);
+    hbxRadio.setAlignment(Pos.CENTER);
 
-    mainPane = new Pane();
-    mainPane.getChildren().add(new VBox(new HBox(lblExpression, txtExpression),
-        new HBox(rbnRevPol, rbnInfix), new HBox(btnCalculate), new HBox(lblResult, txtResult)));
+    HBox hbxButton = new HBox(btnCalculate);
+    hbxButton.setPadding(thickInset);
+    hbxButton.setAlignment(Pos.CENTER);
+
+    HBox hbxResult = new HBox(lblResult, txtResult);
+    hbxResult.setPadding(thickInset);
+    hbxResult.setAlignment(Pos.CENTER);
+
+    VBox mainVBox = new VBox(hbxExpression, hbxRadio, hbxButton, hbxResult);
+    mainVBox.setPrefWidth(600f);
+    mainVBox.setPrefHeight(400f);
+    mainPane = new Pane(mainVBox);
 
     Scene scene = new Scene(mainPane, 600, 400);
     primaryStage.setScene(scene);
